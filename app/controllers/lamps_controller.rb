d5d3ae6ebad86024b10dfba64fde6509455bbfe1
Mattias Nilsen <kerp@chalmers.it>
2015-03-17 23:40:10 +0100

    Added lamp controller and some code to print information about lamps

---
+++ app/controllers/lamps_controller.rb
@@ -0,0 +1,33 @@
+class LampsController < ApplicationController
+
+	def index
+		@client = Hue::Client.new
+
+		@lights = @client.lights
+
+#		client = Hue::Client.new
+
+		for light in @lights
+			light.set_state({
+				:hue => [0,12750,36210,46920,56100].sample,
+				:saturation => 254
+				}, 0)
+		end
+	end
+
+	def edit
+		client = Hue::Client.new
+
+		for light in client.lights
+			light.set_state({
+				:hue => [0,12750,36210,46920,56100].sample,
+				:saturation => 254
+				}, 0)
+		end
+	end
+
+	def show
+
+	end
+
+end
---
+++ test/controllers/lamps_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LampsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
