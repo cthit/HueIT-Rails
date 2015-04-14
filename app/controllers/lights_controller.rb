429825dc09c9dd403253ec37a817be11e31bff80
Kevin Hoogendijk <kevinhoog@gmail.com>
2015-04-14 22:16:27 +0200

    Cleans up code in lights controller

--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -1,11 +1,6 @@
 class LightsController < ApplicationController
 	def index
-   # require 'huey'
     @lights = Huey::Bulb.all
-   # for light in @lights
-   #   light.update(transitiontime: 0, 
-   #             hue: [0, 12750, 36210, 46920, 56100].sample)
-   # end
   end
 
 	def edit
