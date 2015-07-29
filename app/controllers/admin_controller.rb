d732bb7bb160bd60ad68df95ba5f7474e2b2d80e
Mattias Nilsen <kerp@chalmers.it>
2015-07-29 15:58:15 +0200

    Made the admin page prettier

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,14 @@
 class AdminController < ApplicationController
 	def index
-		@log_entries = LogEntry.all
+		@log_entries = LogEntry.all.reverse_order
+		@is_locked = locked?
+	end
+
+	def lock
+		index
+	end
+
+	def locked?
+		true
 	end
 end
