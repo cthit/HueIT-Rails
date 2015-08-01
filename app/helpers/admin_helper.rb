5ccad2aa9d78e5bfa65b22e7238f64401ce93c3e
oskar367 <oskar.jedvert@gmail.com>
2015-08-01 18:27:19 +0000

    added current_user and active_booking to AdminHelper

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,29 @@
 module AdminHelper
+
+	#needs to be refactored(works at least)
+	def active_booking
+		token = "29819a3828fbf0c957a16ce8c95a6129"
+		url = "https://bookit.chalmers.it/current.json"
+		
+		response = HTTParty.get(
+  			url, 
+  			headers: {"Authorization" => "Token token=#{token}"}
+		)
+		booking = JSON.parse(response.body)
+		@group = booking["group"]
+		@exp_date = booking["end_date"]
+		booking["group"]
+	end
+
+  	def current_user
+  		if session[:cookie] == cookies[:chalmersItAuth] && session[:user].present?
+  			@user ||= User.find(session[:user])
+		else
+			reset_session
+			@user = User.find_by_token cookies[:chalmersItAuth]
+			session[:cookie] = cookies[:chalmersItAuth]
+			session[:user] = @user.cid
+  	  	end
+  		@user
+  	end
 end
