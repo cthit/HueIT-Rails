924a1bb53012a92c44429d21837c1a7e3344b057
oskar367 <oskar.jedvert@gmail.com>
2015-08-01 18:32:17 +0000

    adds lock functionality

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,44 @@
-class AdminController < ApplicationController
+class AdminController < ActionController::Base
+	include AdminHelper
+  	helper_method :current_user
+
+  	rescue_from SecurityError, with: :not_signed_in
 	def index
-		@log_entries = LogEntry.all
+		#Gets entries from last 3 hours
+		@log_entries = LogEntry.where("created_at >= ?", Time.now - 3.hour)
 	end
+
+	def lock
+		#Call to active_booking to get @group, can this be done less ugly?
+		active_booking
+		if current_user.in_group?(@group) || current_user.admin?
+			#If the params of url are valid
+			if params[:lock_type].eql?("only_group") || params[:lock_type].eql?("only_admin") || params[:lock_type].eql?("unlocked")
+				#Create new lockstate using params
+				lock_state = LockState.new
+				lock_state.state = params[:lock_type]
+				if @group 
+					lock_state.group = @group
+					lock_state.expiration_date = @exp_date
+				else
+					lock_state.group = "digit"
+					lock_state.expiration_date = DateTime.now + 1.hour
+				end
+				lock_state.user = current_user.cid
+				lock_state.save
+				#To print later
+				@lock_type = params[:lock_type]
+			else
+				@lock_type = "INVALID TYPE"
+			end  
+		end
+	end
+	
+    def allow_iframe
+      response.headers['X-Frame-Options'] = 'ALLOW-FROM https://chalmers.it'
+    end
+
+    def not_signed_in
+      render text: 'Logga in på: <a href="https://account.chalmers.it" target="_top">https://account.chalmers.it</a>'
+    end
 end
--- app/controllers/lights_controller.rb
+++ app/controllers/lights_controller.rb
@@ -1,8 +1,11 @@
 class LightsController < ApplicationController
+	include AdminHelper
+
+	before_action :check_lock_state, except: [:index]
+
 	def change_logger
 		@@change_logger ||= Logger.new("#{Rails.root}/log/change.log")
 	end
-
 #Creates sites
 	def index
 		begin
@@ -147,11 +150,29 @@ class LightsController < ApplicationController
 		@light.save
 		log("Lamp ##{params[:id]} toggled")
 	end
+	
+	private
 	def log(change)
 		entry = LogEntry.new
 		@user = User.find_by_token cookies[:chalmersItAuth]
 		entry.cid = @user.cid
 		entry.change = change
 		entry.save
+	end
+
+	def check_lock_state
+		#Gets all lockstates orders by creation and gets the latest one
+		latest_lock = LockState.order(:created_at).last
+		#if exp date is later than current time
+		if latest_lock.expiration_date >= Time.now
+			#checks lock state
+			if latest_lock.state.eql?("only_group") || latest_lock.state.eql?("only_admin")
+				#call to active booking to get @group (can this be done better?)
+				active_booking
+				unless current_user.in_group(@group) || current_user.admin?
+					render :file => "app/views/lights/lock.html", :status => :unauthorized
+				end
+			end
+		end
 	end
 end
