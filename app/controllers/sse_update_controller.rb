1b356059f2104312b2704bf1c43afd1abdc4389a
Kalior <kalior@chalmers.it>
2015-10-07 14:26:19 +0200

    Added an sse streamer, currently sends data every second which needs to be fixed

--- app/controllers/sse_update_controller.rb
+++ app/controllers/sse_update_controller.rb
@@ -1,2 +1,31 @@
+require 'json'
 class SseUpdateController < ApplicationController
+  include ActionController::Live
+
+  def index
+    response.headers['Content-Type'] = 'text/event-stream'
+    while true do
+        lights = Huey::Bulb.all
+
+        hue_array = Array.new
+    		sat_array = Array.new
+
+    		lights.each_with_index do |light, i|
+    			hue_array[i] = light.hue
+    			sat_array[i] = light.sat
+    		end
+
+        data = {hue: hue_array, sat: sat_array}
+
+        response.stream.write 'data: ' + data.to_json + "\n\n"
+
+      sleep 1
+    end
+
+    rescue IOError
+      # Client Disconnected
+    ensure
+      response.stream.close
+  end
+
 end
