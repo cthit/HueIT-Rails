387ce290ad61cf6d675f44b3ac45eb3f9f926dbe
Criticalstone <kevinhoog@gmail.com>
2016-02-07 21:26:34 +0100

    Party mode now changes color in address bar on android chrome v39+

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,8 +24,8 @@ function switchOnOff(i,hue,sat,bri) {
 	});
 }
 //Converts to color HSB object (code from here http://www.csgnetwork.com/csgcolorsel4.html with some improvements)
-function rgb2hsb(r, g, b){    
-	r /= 255; g /= 255; b /= 255; // Scale to unity.   
+function rgb2hsb(r, g, b){
+	r /= 255; g /= 255; b /= 255; // Scale to unity.
 	var minVal = Math.min(r, g, b),
 	maxVal = Math.max(r, g, b),
 	delta = maxVal - minVal,
@@ -42,7 +42,7 @@ function rgb2hsb(r, g, b){
 		else if (g === maxVal) {HSB.hue = (1 / 3) + del_R - del_B;}
 		else if (b === maxVal) {HSB.hue = (2 / 3) + del_G - del_R;}
 
-		if (HSB.hue < 0) {HSB.hue += 1;}		
+		if (HSB.hue < 0) {HSB.hue += 1;}
 		if (HSB.hue > 1) {HSB.hue -= 1;}
 	}
 
@@ -101,7 +101,7 @@ function runParty(){
 	 		x+=2;
 	 		if(x>=20){
 	 			x=-100;
-	 		}	
+	 		}
 	 	};
    	loopInterval = setInterval(partyLoop, delta);
 	}
@@ -158,7 +158,40 @@ var ready = function() {
 	draw_hue_canvas();
 	draw_sat_canvas();
 	draw_bri_canvas();
+	var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+	if(userAgent.match(/Android/i)) {
+		changeThemeColor();
+	}
 	// sse_waiter();
+}
+
+var changeThemeColor = function() {
+		var colors = ["#09cdda", "#9306ff", "#ff066f", "#0658ff", "#06ff93", "#b0ff06", "#ffcd06" ];
+		var time = 250;
+		var i = 0;
+		var party = false;
+		var defaultColor = "#09cdda";
+		var changeColor = function() {
+			$('meta[name=theme-color]').remove();
+			$('head').append('<meta name="theme-color" content="' + colors[i] + ' ">');
+			i++;
+			if ( i == colors.length) {
+				i = 0;
+			}
+			if (party) {
+				setTimeout(changeColor, time);
+			} else {
+				$('meta[name=theme-color]').remove();
+				$('head').append('<meta name="theme-color" content="' + defaultColor +'" >');
+			}
+		};
+		$('#party_btn').click(function() {
+			party = !party;
+			if (party) {
+				setTimeout(changeColor, time);
+			} else {
+			}
+		});
 }
 
 $(document).on('page:load', ready);
