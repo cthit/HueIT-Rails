20a791b0a44a1f6639125ac59083d730c4a2d796
Kalior <kalioragus@gmail.com>
2017-04-28 21:22:25 +0200

    Use RGBtoCSS function

--- app/assets/javascripts/draw_color_shower.js
+++ app/assets/javascripts/draw_color_shower.js
@@ -57,7 +57,7 @@ function draw (id, hue, sat, brightness) {
   var div = document.getElementById('color_shower_' + id)
 
   var rgb = HSVtoRGB(hue, sat, brightness)
-  div.style.background = 'rgb(' + rgb[0] + ',' + rgb[1] + ',' + rgb[2] + ')'
+  div.style.background = RGBtoCSS(rgb)
 }
 
 // Used when disregarding the value of the selectors, only wanting to draw the color of the bulb or if it is off
