466fe9ca3ce0836c429e447743b1dcf0d3870632
Kalior <kalioragus@gmail.com>
2017-04-28 22:59:30 +0200

    Update sse functions to support new indexing

--- app/controllers/sse_update_controller.rb
+++ app/controllers/sse_update_controller.rb
@@ -8,20 +8,10 @@ class SseUpdateController < ApplicationController
     while true do
       # If the int has changed we are told to update
       if change_int != Rails.application.config.sse_int
-        change_int = Rails.application.config.sse_int 
+        change_int = Rails.application.config.sse_int
         lights = Huey::Bulb.all
 
-        hue_array = Array.new
-    		sat_array = Array.new
-
-    		lights.each_with_index do |light, i|
-    			hue_array[i] = light.hue
-    			sat_array[i] = light.sat
-    		end
-
-        data = {hue: hue_array, sat: sat_array}
-
-        response.stream.write 'data: ' + data.to_json + "\n\n"
+        response.stream.write 'data: ' + lights.to_json + "\n\n"
       end
     end
 
